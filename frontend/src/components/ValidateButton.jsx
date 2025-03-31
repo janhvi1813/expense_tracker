6d289c25e27ba638180650cc62ecba2843ed6c12
garvitjawa <gaminggarvit@gmail.com>
2025-03-31 23:57:02 +0530

    Frontend tweaks

--- frontend/src/components/ValidateButton.jsx
+++ frontend/src/components/ValidateButton.jsx
@@ -4,7 +4,7 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const ValidateButton = () => {
-  const [isValidated, setIsValidated] = useState(false); 
+  const [isValidated, setIsValidated] = useState(false);
 
   const handleValidation = async () => {
     try {
@@ -12,7 +12,7 @@ const ValidateButton = () => {
       const data = await response.json();
 
       if (data === true) {
-        setIsValidated(true); 
+        setIsValidated(true);
         toast.success("✅ Transaction is valid!");
       } else {
         setIsValidated(false);
@@ -26,26 +26,43 @@ const ValidateButton = () => {
 
   return (
     <>
-      <Container display="flex" justifyContent="center" alignItems="center" mt={50} mb={-150}>
+      <Container
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        mt={50}
+        mb={-150}
+      >
         <Box bg="#F6F1DE" w="100%" p="4" color="white" minH="17vh">
-          <Text color="black" textStyle="3xl" fontWeight={"medium"}>Validate your transactions</Text>
+          <Text color="black" textStyle="3xl" fontWeight={"medium"}>
+            Validate your transactions
+          </Text>
           <HStack>
-            <Button variant="outline" size="lg" onClick={handleValidation} rounded="14" bg="black" color="white" mt={2.5}>
+            <Button
+              variant="outline"
+              size="lg"
+              onClick={handleValidation}
+              rounded="14"
+              bg="black"
+              color="white"
+              mt={2.5}
+            >
               Validate Transaction
             </Button>
           </HStack>
           {/* ✅ Show this box only when validation is successful */}
           {isValidated && (
-            <Box mt={2} p={3} bg="green.500" color="white" fontWeight="bold" borderRadius="md" >
+            <Box
+              mt={2}
+              p={3}
+              bg="green.500"
+              color="white"
+              fontWeight="bold"
+              borderRadius="md"
+            >
               ✅ Your blockchain of transactions is validated!
             </Box>
           )}
-           {!isValidated && (
-            <Box mt={2} p={3} bg="red.400" color="white" fontWeight="bold" borderRadius="md" >
-              ⚠️ Your blockchain of transactions is not validated!
-            </Box>
-          )}
-
         </Box>
       </Container>
       {/* Toast Message Container */}
